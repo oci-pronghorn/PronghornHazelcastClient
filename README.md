deae89a7625228426ffa58a5f6540bfe98070965
Nathan Tippy <nathan@tippy.name>
2015-08-30 17:21:08 -0500

    First check in for hazelcast client

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PronghornHazelcastClient
 Smart Client for Hazelcast using the Pronghorn lock free actor framework
+
+Under active development
---
+++ src/main/java/com/ociweb/hazelcast/HZClient.java
@@ -0,0 +1,33 @@
+package com.ociweb.hazelcast;
+
+public class HZClient {
+
+        
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        
+    }
+    
+    
+    /*
+     * 
+     * 
+     * 
+     * 
+     */
+    
+    
+    public static class Client {
+        
+        //TODO: insert all the generated methods here.
+        
+        public static void doIt(Object state) {
+            
+        }
+        
+    }
+    
+    
+}
