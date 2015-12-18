ff3643157d14197b88fa1cd4229741cd82364ce3
Charles Sharp <cas.nixster@gmail.com>
2015-12-18 16:33:47 -0600

    Snapshot current work

---
+++ src/test/java/com/ociweb/hazelcast/HazelcastSetTestX.java
@@ -0,0 +1,47 @@
+package com.ociweb.hazelcast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.*;
+
+public class HazelcastSetTestX {
+
+
+    private static HazelcastClientConfig config;
+    private static HazelcastClient client;
+
+    public static void main(String[] args) {
+        config = new HazelcastClientConfig("path to config");
+        client = new HazelcastClient(config);
+        int setSize = -1;
+
+        HazelcastResponseHandler responseHandler = new HazelcastResponseHandler();
+
+        int cid = 1;
+
+//        HazelcastClient.registerCallBack(config, cid, responseHandler);
+//        //For monitoring or getting all responses from cluster
+//        HazelcastClient.registerCallBackMonitorAll(config, responseForAll);
+//        //For only getting those that do not have a CallBack for the correlation id registered
+//        HazelcastClient.registerCallBackDefault(config, responseForWhenCorrelationIdNotFound);
+//
+//        //at some point to remove listener can call
+//        HazelcastClient.clearCallBack(config, cid);
+
+
+        int fstoken = HazelcastSet.newSet(client, cid, "FirstSet");
+        // Add a string, Must be serializable or identifiable serializble or portable..
+        HazelcastSet.add(client, cid, fstoken, "MyStringValue");
+
+        // Add a low level object, can be used for very tight serialization
+/*
+        DataOutputStream out = HazelcastSet.add(client, cid, fstoken);
+        out.writeLong(42);
+        out.close();
+*/
+
+        //request the size and the callback will get the response
+        HazelcastSet.size(client, cid, fstoken);
+    }
+}
